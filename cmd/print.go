3c11b9e66e395f9f73c24da629e53c92d6d5ea9a
Felix Hoffmann <19827840+zzzFelix@users.noreply.github.com>
2023-10-15 21:31:46 +0200

    feat: print summary from multiple days

--- cmd/print.go
+++ cmd/print.go
@@ -1,8 +1,6 @@
 package cmd
 
 import (
-	"time"
-
 	"github.com/spf13/cobra"
 	"github.com/zzzFelix/gotrack/timespan"
 	"github.com/zzzFelix/gotrack/util"
@@ -13,22 +11,24 @@ func init() {
 }
 
 var printCmd = &cobra.Command{
-	Use:   "print [date]",
-	Short: "Prints times for a given date.",
-	Long: `Prints times for a given date. When no date given, today's times are printed. Examples:
+	Use:   "print [(start) date] [end date]",
+	Short: "Prints times for given date(s).",
+	Long: `Prints times for a given time period. When no date given, today's times are printed. Examples:
+	'gotrack print 2023-05-01 2023-05-30' -- prints working hours from 1st May 2023 to 30th May 2023 (both inclusive).
 	'gotrack print 2023-05-01' -- prints working hours for 1st May 2023.
 	'gotrack print' -- prints working hours for today.`,
-	Args: cobra.MatchAll(cobra.MaximumNArgs(1), cobra.OnlyValidArgs),
+	Args: cobra.MatchAll(cobra.MaximumNArgs(2), cobra.OnlyValidArgs),
 	Run: func(cmd *cobra.Command, args []string) {
-		ts := timespan.Timespan{}
-		date := time.Now()
 		switch len(args) {
 		case 0:
-			ts = timespan.GetToday()
+			timespan.GetToday()
 		case 1:
-			date = util.ParseDate(args[0])
-			ts = timespan.Get(date)
+			date := util.ParseDate(args[0])
+			timespan.Get(date)
+		case 2:
+			start := util.ParseDate(args[0])
+			end := util.ParseDate(args[1])
+			timespan.GetMultipleDays(start, end)
 		}
-		ts.Print(date)
 	},
 }
