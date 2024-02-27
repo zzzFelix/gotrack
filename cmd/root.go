7e0515c27c86747352ee1f71df7fe998ecdf1536
Felix Hoffmann <19827840+zzzFelix@users.noreply.github.com>
2024-02-27 20:52:29 +0100

    better cli docs

--- cmd/root.go
+++ cmd/root.go
@@ -3,11 +3,25 @@ package cmd
 import (
 	"fmt"
 	"os"
+
+	"github.com/spf13/cobra"
 )
 
 func Execute() {
-	if err := saveCmd.Execute(); err != nil {
+	if err := rootCmd.Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 }
+
+var (
+	rootCmd = &cobra.Command{
+		Run: func(cmd *cobra.Command, args []string) {
+			if len(args) == 0 || len(args) > 4 {
+				saveCmd.Help()
+			} else {
+				saveCmd.Execute()
+			}
+		},
+	}
+)
